72da89e3eab483e61e5b5598c519d46d783fdcc9
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2025-03-11 23:20:59 +0530

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Remove any existing server.pid file
 rm -f /app/tmp/pids/server.pid
 
-# Run database migrations
-bundle exec rails db:migrate
+# Load logger before starting the application
+echo "require 'logger'" > /app/config/initializers/logger.rb
 
 # Then exec the container's main process
 exec "$@"
